7fd66342107f137f0a41bce9ec7a2d42df5438aa
Алинка <alinaaaiuiuiii@gmail.com>
2025-10-18 14:13:39 +0300

    feat: add interactive elements

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,16 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+void _onPressed() {
+    print('Button pressed!');
+}
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(onPressed: _onPressed,
+        backgroundColor: Colors.pink[100],),
         body: Column (
           children: [
             Container(width: 500, height: 100, color: Colors.pink[200],),
@@ -21,10 +27,19 @@ class MainApp extends StatelessWidget {
               Text('текст3'),
             ],
             ),
-            Container(width: 1000, height: 100, color: Colors.blue[300],)
+            Container(width: 1000, height: 100, color: Colors.blue[300],),
+            Expanded(child: 
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CircleAvatar(radius: 90, backgroundImage: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),),
+                CircleAvatar(radius: 50, backgroundColor: Colors.yellow[400])
+                ],
+            ))
           ],
         ),
       ),
     );
   }
 }
+       
