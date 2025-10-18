7925a2d175755f1aee5c7eb97ab57494b882d7db
Алинка <alinaaaiuiuiii@gmail.com>
2025-10-18 12:20:56 +0300

    feat: add basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Column (
+          children: [
+            Container(width: 500, height: 100, color: Colors.pink[200],),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text('текст1'),
+              Text('текст2'),
+              Text('текст3'),
+            ],
+            ),
+            Container(width: 1000, height: 100, color: Colors.blue[300],)
+          ],
         ),
       ),
     );
