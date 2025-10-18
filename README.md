eeb2495a1fcab29533f73610d6d71e68ccb912c3
Алинка <alinaaaiuiuiii@gmail.com>
2025-10-18 11:50:27 +0300

    Первый коммит: добавлен скриншот flutter doctor

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_laboratornaya_1
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_laboratornaya_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_laboratornaya_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
